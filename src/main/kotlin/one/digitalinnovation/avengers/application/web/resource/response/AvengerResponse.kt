a905fc914a63673df6f542ed98b234c7edc530a5
rsbastos <renedossantosbastos@gmail.com>
2024-02-29 10:25:48 -0300

    :construction: packages and files structure

---
+++ src/main/kotlin/one/digitalinnovation/avengers/application/web/resource/response/AvengerResponse.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.avengers.application.web.resource.response
+
+data class AvengerResponse(
+    val nick: String,
+    val person: String,
+    val description: String,
+    val history: String
+)
---
+++ src/main/kotlin/one/digitalinnovation/avengers/domain/avenger/Avenger.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.avengers.domain.avenger
+
+data class Avenger(
+    val nick: String,
+    val person: String,
+    val description: String,
+    val history: String
+)
